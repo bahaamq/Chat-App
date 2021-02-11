95c0fdd73c7200db5d5b29a10f90f4a55324f90f
bahaamq <bahaa.mq@gmail.com>
2021-02-11 12:35:02 +0200

    update

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ socket.on('disconnect',()=>{
 
   })
 
-  server.listen(process.env.port || 3000 ,()=>{
+  server.listen(process.env.PORT || 3000 ,()=>{
 
     console.log('Running at Port 3000');
 
